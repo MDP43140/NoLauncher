9ba6e4c6afa4d114b029a1e5ccb42408a1bc1cd7
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2022-12-10 12:20:02 +0700

    Update files
    + [ItemApp.xml] Replace 4 space to tab, and change margin to 12dp.
    + Remove blank space.
    + [MainActivity] Remove onlongclick.
    + [MainActivity] Remove unnesecary convertView.
    + Replace `) {` > `){`.
    + [MainActivity] Remove unnesecary JavaLocale dependency and codes in search().
    + Simplify OnBackPressed,OnResume function.
    + Removing some unnesecary try catch codes (where errors are rare).

--- app/src/main/java/io/mdp43140/nolauncher/AppAdapter.java
+++ app/src/main/java/io/mdp43140/nolauncher/AppAdapter.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
@@ -15,28 +14,16 @@ import android.widget.TextView;
 public class AppAdapter extends BaseAdapter {
 	private Activity activity;
 	private ArrayList<AppItem> apps;
-
-	AppAdapter(Activity activity, ArrayList<AppItem> apps) {
+	AppAdapter(Activity activity, ArrayList<AppItem> apps){
 		this.activity = activity;
 		this.apps = apps;
 	}
-
-	@Override public int getCount() {
-		return apps.size();
-	}
-
-	@Override public Object getItem(int position) {
-		return apps.get(position);
-	}
-
-	@Override public long getItemId(int position) {
-		return position;
-	}
-
-	@Override public View getView(int position, View convertView, ViewGroup parent) {
-		View view = convertView;
+	@Override public int getCount(){return apps.size();}
+	@Override public Object getItem(int position){return apps.get(position);}
+	@Override public long getItemId(int position){return position;}
+	@Override public View getView(int position, View view, ViewGroup parent){
 		final ViewHolder holder;
-		if (convertView == null) {
+		if (view == null){
 			view = activity.getLayoutInflater().inflate(R.layout.item_app, null);
 			holder = new ViewHolder();
 			holder.item_app_name = view.findViewById(R.id.item_app_name);
@@ -47,32 +34,15 @@ public class AppAdapter extends BaseAdapter {
 		}
 		final AppItem app = (AppItem) getItem(position);
 		holder.item_app_icon.setImageDrawable(app.icon);
-		view.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
+		view.setOnClickListener(new OnClickListener(){
+			@Override public void onClick(View v){
 				v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
-				try {
-					activity.startActivity(activity.getPackageManager().getLaunchIntentForPackage(app.packageName));
-				} catch (Exception e) {
-				}
-			}
-		});
-		view.setOnLongClickListener(new OnLongClickListener() {
-
-			@Override
-			public boolean onLongClick(View v) {
-				v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
-				try {
-					activity.startActivity(new Intent(Intent.ACTION_DELETE, Uri.parse("package:" + app.packageName)));
-				} catch (Exception ignored) {
-				}
-				return false;
+				activity.startActivity(activity.getPackageManager().getLaunchIntentForPackage(app.packageName));
 			}
 		});
 		holder.item_app_name.setText(app.name);
 		return view;
 	}
-
 	public class ViewHolder {
 		ImageView item_app_icon;
 		TextView item_app_name;
--- app/src/main/java/io/mdp43140/nolauncher/MainActivity.java
+++ app/src/main/java/io/mdp43140/nolauncher/MainActivity.java
@@ -12,32 +12,21 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 public class MainActivity extends Activity {
 	@SuppressLint("StaticFieldLeak")
 	public static MainActivity self;
-
+	private static ArrayList<AppItem> apps;
 	private EditText activity_home_search;
 	private ListView activity_home_list;
-	private static ArrayList<AppItem> apps;
 
 	@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		activity_home_search = findViewById(R.id.activity_home_search);
 		activity_home_search.addTextChangedListener(new TextWatcher() {
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				search(s.toString());
-			}
-
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-			}
-
-			@Override
-			public void afterTextChanged(Editable s) {
-			}
+			@Override public void onTextChanged(CharSequence s, int start, int before, int count) {search(s.toString());}
+			@Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+			@Override public void afterTextChanged(Editable s) {}
 		});
 		activity_home_list = findViewById(R.id.activity_home_list);
 
@@ -48,57 +37,43 @@ public class MainActivity extends Activity {
 		intentFilter.addDataScheme("package");
 		registerReceiver(new AppChangeReceiver(), intentFilter);
 	}
-
 	@Override protected void onResume() {
 		super.onResume();
 		self = this;
-		if (apps == null || apps.size() == 0)
-			loadApps();
+		if (apps == null) loadApps();
 	}
-
 	@Override protected void onStop() {
 		super.onStop();
 		self = null;
 	}
-
 	@Override public void onBackPressed() {
-		try {
-			if (activity_home_search.getText().toString().length() > 0)
-				activity_home_search.setText("");
-		} catch (Exception ignored) {
-		}
+		activity_home_search.setText("");
 	}
 
 	public void loadApps() {
-		try {
-			apps = new ArrayList<>();
-			Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-			mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-			List<ResolveInfo> pkgAppsList = getPackageManager().queryIntentActivities(mainIntent, 0);
-			for (ResolveInfo info : pkgAppsList) {
-				AppItem app = new AppItem();
-				app.name = info.activityInfo.applicationInfo.loadLabel(getPackageManager()).toString();
-				app.packageName = info.activityInfo.applicationInfo.packageName;
-				app.icon = info.activityInfo.applicationInfo.loadIcon(getPackageManager());
-				apps.add(app);
-			}
-			Collections.sort(apps);
-			AppAdapter adapter = new AppAdapter(MainActivity.this, apps);
-			activity_home_list.setAdapter(adapter);
-		} catch (Exception ignored) {
+		apps = new ArrayList<>();
+		Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		List<ResolveInfo> pkgAppsList = getPackageManager().queryIntentActivities(mainIntent, 0);
+		for (ResolveInfo info: pkgAppsList) {
+			AppItem app = new AppItem();
+			app.name = info.activityInfo.applicationInfo.loadLabel(getPackageManager()).toString();
+			app.packageName = info.activityInfo.applicationInfo.packageName;
+			app.icon = info.activityInfo.applicationInfo.loadIcon(getPackageManager());
+			apps.add(app);
 		}
+		Collections.sort(apps);
+		AppAdapter adapter = new AppAdapter(MainActivity.this, apps);
+		activity_home_list.setAdapter(adapter);
 	}
 	private void search(String text) {
-		try {
-			ArrayList<AppItem> resultApps = new ArrayList<>();
-			for (AppItem app : apps) {
-				if (app.name.toLowerCase(Locale.getDefault()).contains(text.toLowerCase(Locale.getDefault()))) {
-					resultApps.add(app);
-				}
+		ArrayList<AppItem> resultApps = new ArrayList<>();
+		for (AppItem app: apps) {
+			if (app.name.toLowerCase().contains(text.toLowerCase())) {
+				resultApps.add(app);
 			}
-			AppAdapter adapter = new AppAdapter(MainActivity.this, resultApps);
-			activity_home_list.setAdapter(adapter);
-		} catch (Exception ignored) {
 		}
+		AppAdapter adapter = new AppAdapter(MainActivity.this, resultApps);
+		activity_home_list.setAdapter(adapter);
 	}
 }
